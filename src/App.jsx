eef57df9e003ff27f93fb0e19ecb73e40e4a3f20
Khanhbroo <khanhmathias532211@gmail.com>
2025-03-19 16:58:48 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+const genRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+const Header = () => {
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+};
+
+const CoreConcept = (props) => {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
